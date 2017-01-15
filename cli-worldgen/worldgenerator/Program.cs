cf07f7611bc52cb1ff57140c29bbf5817ab99735
Kevin <kevin.mellachew@gmail.com>
2017-01-14 23:08:27 -0500

    Created a basic structure for the project

---
+++ cli-worldgen/worldgenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worldgenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Currently a work in progress...");
+            Console.ReadLine();
+        }
+    }
+}
